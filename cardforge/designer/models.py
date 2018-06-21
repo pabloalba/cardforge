cf638c004b5e3b25a606a17614f47057bdc4eb2c
Pablo Alba <pablo.alba@kaleidos.net>
2018-06-21 18:47:43 +0200

    Deck model

--- cardforge/designer/models.py
+++ cardforge/designer/models.py
@@ -10,3 +10,29 @@ class Game(models.Model):
 
     class Meta:
         ordering = ('name',)
+
+
+class Deck(models.Model):
+    DECK_SIZES = (
+        ('S', 'standard'),
+        ('M', 'mini'),
+        ('B', 'big'),
+        ('R', 'bridge'),
+        ('Q', 'squared'),
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100, blank=False, null=False)
+    icon = models.CharField(max_length=255, blank=True)
+    game = models.ForeignKey(Game, related_name='decks')
+    size = models.CharField(max_length=1, choices=DECK_SIZES)
+    same_reverse = models.BooleanField(null=False, default=True)
+    front_border_color = models.CharField(max_length=7, blank=False, null=False, default="#000000")
+    front_cut_marks_color = models.CharField(max_length=7, blank=False, null=False, default="#FF0000")
+    back_border_color = models.CharField(max_length=7, blank=False, null=False, default="#000000")
+    back_cut_marks_color = models.CharField(max_length=7, blank=False, null=False, default="#FF0000")
+    cards = models.TextField()  # json data
+    front_layers = models.TextField()  # json data
+    back_layers = models.TextField()  # json data
+
+    class Meta:
+        ordering = ('name',)
--- cardforge/designer/serializers.py
+++ cardforge/designer/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from designer.models import Game
+from designer.models import Deck, Game
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -14,3 +14,8 @@ class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
         fields = ('id', 'name', 'icon')
+
+
+class DeckSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Deck
