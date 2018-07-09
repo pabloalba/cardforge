b3fabace6184ca949a0594592069bf292e03b35b
Pablo Alba <pablo.alba@kaleidos.net>
2018-07-09 15:06:26 +0200

    Layer card generation test

--- cardforge/designer/image.py
+++ cardforge/designer/image.py
@@ -1,55 +1,6 @@
 from PIL import Image, ImageDraw
 
-size_troquel_mini = {
-    'NAME': 'mini',
-    'WIDTH': 4960,
-    'HEIGHT': 3507,
-    'BOX_WIDTH': 597,
-    'BOX_HEIGHT': 822,
-    'MARGIN_X': 73,
-    'MARGIN_Y': 73,
-    'GAP_X': 106,
-    'GAP_Y': 25,
-    'CARDS_PER_ROW': 7,
-    'CUT_MARK_DISPLACEMENT': 35,
-    'CUT_MARK_OVERLAP': 23,
-    'CUT_MARK_SIZE': 37,
-    'CUT_MARKS': True,
-    'ROTATE': True
-}
-
-size_troquel_standard = {
-    'NAME': 'standard',
-    'WIDTH': 4960,
-    'HEIGHT': 3507,
-    'BOX_WIDTH': 811,
-    'BOX_HEIGHT': 1106,
-    'MARGIN_X': 113,
-    'MARGIN_Y': 78,
-    'GAP_X': 170,
-    'GAP_Y': 17,
-    'CARDS_PER_ROW': 5,
-    'CUT_MARK_DISPLACEMENT': 33,
-    'CUT_MARK_OVERLAP': 9,
-    'CUT_MARK_SIZE': 62,
-    'CUT_MARKS': True,
-    'ROTATE': True
-}
-
-size_tabletop_mini = {
-    'NAME': 'standard',
-    'WIDTH': 5080,
-    'HEIGHT': 5124,
-    'BOX_WIDTH': 508,
-    'BOX_HEIGHT': 732,
-    'MARGIN_X': 0,
-    'MARGIN_Y': 0,
-    'GAP_X': 0,
-    'GAP_Y': 0,
-    'CARDS_PER_ROW': 10,
-    'CUT_MARKS': False,
-    'ROTATE': True
-}
+from designer.sizes import size_tabletop_mini, size_troquel_mini, size_troquel_standard
 
 
 def create_image(size):
@@ -176,4 +127,6 @@ def draw_cut_mark(draw, size, pos_x, pos_y, vertical=True):
 
 
 if __name__ == "__main__":
+    create_image(size_tabletop_mini)
     create_image(size_troquel_mini)
+    create_image(size_troquel_standard)
---
+++ cardforge/designer/layers.py
@@ -0,0 +1,53 @@
+from io import BytesIO
+
+import requests
+from PIL import Image, ImageDraw
+
+from .sizes import size_troquel_mini
+
+
+class Layer:
+    def __init__(self, type, enabled=True, x=0, y=0, file=None, font=None, font_size=None, text_align=None,
+                 font_weight=None, color=None, text=""):
+        self.type = type
+        self.enabled = enabled
+        self.x = x
+        self.y = y
+        self.file = file
+        self.font = font
+        self.font_size = font_size
+        self.text_align = text_align
+        self.font_weight = font_weight
+        self.color = color
+        self.text = text
+
+
+def create_card(size, layers):
+    im = Image.new("RGBA", (825, 600))
+    draw = ImageDraw.Draw(im)
+    for layer in layers:
+        if layer.type == "image":
+            file = layer.file
+            file = file.replace("www.dropbox.com", "dl.dropboxusercontent.com")
+
+            response = requests.get(file)
+            image = Image.open(BytesIO(response.content))
+            im.paste(image, (layer.x, layer.y), image)
+        elif layer.type == "text":
+            # font = ImageFont.truetype("sans-serif.ttf", 16)
+
+            draw.text((layer.x, layer.y), layer.text, layer.color)
+
+    im.save("card.png")
+
+
+if __name__ == "__main__":
+    layers = [
+        Layer(type="background", color="#FF0000"),
+        Layer(type="image", file="https://www.dropbox.com/s/vrpgh14b1kw271s/base.png?dl=0"),
+        Layer(type="image", file="https://www.dropbox.com/s/a8aeycs0fqckqfw/circle.png?dl=0"),
+        Layer(type="image", file="https://www.dropbox.com/s/00ei8pzgcfsg5p3/paca.png?dl=0"),
+        Layer(type="text", font="", font_size=48, color="#00FF00", text="Paca", x=100, y=50),
+    ]
+
+    create_card(size_troquel_mini, layers)
