09865bd1cb7d93a20a51d9d3a74e6815708013ff
Andrey Antukh <niwi@niwi.nz>
2018-07-17 13:12:27 +0200

    Add cardbuilder component placeholder.

---
+++ front/src/components/cardbuilder/CardBuilder.vue
@@ -0,0 +1,10 @@
+<template lang="pug" src="./CardBuilder.pug"></template>
+
+<script>
+export default {
+  name: 'CardBuilder',
+  data () {
+    return {};
+  }
+}
+</script>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue"
 import Router from "vue-router"
 import Home from "@/components/home/Home"
 import Login from "@/components/login/Login"
+import CardBuilder from "@/components/cardbuilder/CardBuilder"
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: "/login",
       name: "Login",
       component: Login
+    },
+    {
+      path: "/cardbuilder",
+      name: "CardBuilder",
+      component: CardBuilder
     }
   ]
 })
