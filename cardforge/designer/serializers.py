c5de22ddeaf6cb0793c85d80df09f8d422db2526
Andrés Moya <andres.moya@kaleidos.net>
2018-07-18 14:28:58 +0200

    Fix

--- cardforge/designer/serializers.py
+++ cardforge/designer/serializers.py
@@ -29,7 +29,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 class GameSerializer(serializers.HyperlinkedModelSerializer):
     owners = serializers.HyperlinkedIdentityField(view_name='game-owners')
     decks = serializers.HyperlinkedIdentityField(view_name='game-decks')
-    n_decks = serializers.IntegerField()
+    n_decks = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Game
@@ -37,7 +37,7 @@ class GameSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class DeckSimpleSerializer(serializers.HyperlinkedModelSerializer):
-    n_cards = serializers.SerializerMethodField()
+    n_cards = serializers.SerializerMethodField(read_only=True)
 
     def get_n_cards(self, obj):
         if obj.cards:
@@ -55,13 +55,9 @@ class DeckSerializer(serializers.HyperlinkedModelSerializer):
     cards = JSONSerializerField()
     front_layers = JSONSerializerField()
     back_layers = JSONSerializerField()
-    n_cards = serializers.SerializerMethodField()
-
-    def get_n_cards(self, obj):
-        return 44
 
     class Meta:
         model = Deck
-        fields = ('id', 'url', 'name', 'n_cards', 'created', 'size', 'front_cut_marks_color',
+        fields = ('id', 'url', 'name', 'created', 'size', 'front_cut_marks_color',
                   'back_cut_marks_color', 'portrait', 'cards', 'front_layers', 'back_layers')
 
