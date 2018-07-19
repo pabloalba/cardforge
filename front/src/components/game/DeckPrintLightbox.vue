f350008423009a2a3f7372190e5a084630669900
Andrés Moya <andres.moya@kaleidos.net>
2018-07-19 11:41:53 +0200

    Operating print lightbox

--- front/src/components/game/DeckPrintLightbox.vue
+++ front/src/components/game/DeckPrintLightbox.vue
@@ -6,11 +6,33 @@ import {CLOSE_LIGHTBOX} from "../../data/store";
 export default {
   computed: {
     lightboxOpen() {
-      return (this.$store.state.lightboxOpen === 'print-deck');
+      return (this.$store.state.lightboxOpen === "print-deck");
     },
 
     isEmpty() {
-      return false;
+      return (this.printingType.length === 0) ||
+        (this.pageSize.length === 0 && this.printingType !== "tabletop") ||
+        (this.fileType.length === 0 && this.printingType !== "tabletop");
+    },
+
+    disableOptions() {
+      return (this.printingType === "tabletop");
+    }
+  },
+  data() {
+    return {
+      printingType: "",
+      pageSize: "",
+      fileType: "",
+    }
+  },
+  watch: {
+    lightboxOpen(newOpen, oldOpen) {
+      if (newOpen) {
+        this.printingType = "";
+        this.pageSize = "";
+        this.fileType = "";
+      }
     }
   },
   methods: {
